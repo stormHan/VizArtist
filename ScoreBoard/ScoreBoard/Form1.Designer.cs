6358c73daf85d68353a76b689c3e712717fc85b5
v_jiaweihan(韩嘉伟) <v_jiaweihan@tencent.com>
2016-04-18 17:59:56 +0800

    a scoreboard demo with Csharp

---
+++ ScoreBoard/ScoreBoard/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace ScoreBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectIp = new System.Windows.Forms.TextBox();
+            this.ConnectPort = new System.Windows.Forms.TextBox();
+            this.Connectbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.A_1 = new System.Windows.Forms.Button();
+            this.A_2 = new System.Windows.Forms.Button();
+            this.A_3 = new System.Windows.Forms.Button();
+            this.B_1 = new System.Windows.Forms.Button();
+            this.B_2 = new System.Windows.Forms.Button();
+            this.B_3 = new System.Windows.Forms.Button();
+            this.cmdLoadscene = new System.Windows.Forms.Button();
+            this.cmdLoadData = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.TextBox();
+            this.lbpath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConnectIp
+            // 
+            this.ConnectIp.Location = new System.Drawing.Point(46, 37);
+            this.ConnectIp.Name = "ConnectIp";
+            this.ConnectIp.Size = new System.Drawing.Size(171, 21);
+            this.ConnectIp.TabIndex = 0;
+            this.ConnectIp.Text = "localhost";
+            // 
+            // ConnectPort
+            // 
+            this.ConnectPort.Location = new System.Drawing.Point(235, 37);
+            this.ConnectPort.Name = "ConnectPort";
+            this.ConnectPort.Size = new System.Drawing.Size(67, 21);
+            this.ConnectPort.TabIndex = 1;
+            this.ConnectPort.Text = "6100";
+            // 
+            // Connectbtn
+            // 
+            this.Connectbtn.Location = new System.Drawing.Point(326, 37);
+            this.Connectbtn.Name = "Connectbtn";
+            this.Connectbtn.Size = new System.Drawing.Size(75, 23);
+            this.Connectbtn.TabIndex = 2;
+            this.Connectbtn.Text = "Connect";
+            this.Connectbtn.UseVisualStyleBackColor = true;
+            this.Connectbtn.Click += new System.EventHandler(this.Connectbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(80, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Team A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("SimSun", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(80, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Team B";
+            // 
+            // A_1
+            // 
+            this.A_1.Location = new System.Drawing.Point(221, 230);
+            this.A_1.Name = "A_1";
+            this.A_1.Size = new System.Drawing.Size(63, 23);
+            this.A_1.TabIndex = 5;
+            this.A_1.Text = "+1";
+            this.A_1.UseVisualStyleBackColor = true;
+            this.A_1.Click += new System.EventHandler(this.A_1_Click);
+            // 
+            // A_2
+            // 
+            this.A_2.Location = new System.Drawing.Point(359, 231);
+            this.A_2.Name = "A_2";
+            this.A_2.Size = new System.Drawing.Size(63, 23);
+            this.A_2.TabIndex = 6;
+            this.A_2.Text = "+2";
+            this.A_2.UseVisualStyleBackColor = true;
+            this.A_2.Click += new System.EventHandler(this.A_2_Click);
+            // 
+            // A_3
+            // 
+            this.A_3.Location = new System.Drawing.Point(487, 231);
+            this.A_3.Name = "A_3";
+            this.A_3.Size = new System.Drawing.Size(63, 23);
+            this.A_3.TabIndex = 7;
+            this.A_3.Text = "+3";
+            this.A_3.UseVisualStyleBackColor = true;
+            this.A_3.Click += new System.EventHandler(this.A_3_Click);
+            // 
+            // B_1
+            // 
+            this.B_1.Location = new System.Drawing.Point(221, 313);
+            this.B_1.Name = "B_1";
+            this.B_1.Size = new System.Drawing.Size(63, 23);
+            this.B_1.TabIndex = 8;
+            this.B_1.Text = "+1";
+            this.B_1.UseVisualStyleBackColor = true;
+            this.B_1.Click += new System.EventHandler(this.B_1_Click);
+            // 
+            // B_2
+            // 
+            this.B_2.Location = new System.Drawing.Point(359, 314);
+            this.B_2.Name = "B_2";
+            this.B_2.Size = new System.Drawing.Size(63, 23);
+            this.B_2.TabIndex = 9;
+            this.B_2.Text = "+2";
+            this.B_2.UseVisualStyleBackColor = true;
+            this.B_2.Click += new System.EventHandler(this.B_2_Click);
+            // 
+            // B_3
+            // 
+            this.B_3.Location = new System.Drawing.Point(487, 313);
+            this.B_3.Name = "B_3";
+            this.B_3.Size = new System.Drawing.Size(63, 23);
+            this.B_3.TabIndex = 10;
+            this.B_3.Text = "+3";
+            this.B_3.UseVisualStyleBackColor = true;
+            this.B_3.Click += new System.EventHandler(this.B_3_Click);
+            // 
+            // cmdLoadscene
+            // 
+            this.cmdLoadscene.Location = new System.Drawing.Point(100, 118);
+            this.cmdLoadscene.Name = "cmdLoadscene";
+            this.cmdLoadscene.Size = new System.Drawing.Size(172, 60);
+            this.cmdLoadscene.TabIndex = 11;
+            this.cmdLoadscene.Text = "Load Scene";
+            this.cmdLoadscene.UseVisualStyleBackColor = true;
+            this.cmdLoadscene.Click += new System.EventHandler(this.cmdLoadscene_Click);
+            // 
+            // cmdLoadData
+            // 
+            this.cmdLoadData.Location = new System.Drawing.Point(391, 118);
+            this.cmdLoadData.Name = "cmdLoadData";
+            this.cmdLoadData.Size = new System.Drawing.Size(172, 60);
+            this.cmdLoadData.TabIndex = 12;
+            this.cmdLoadData.Text = "Load Data";
+            this.cmdLoadData.UseVisualStyleBackColor = true;
+            this.cmdLoadData.Click += new System.EventHandler(this.cmdLoadData_Click);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(100, 184);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(260, 21);
+            this.path.TabIndex = 13;
+            this.path.Text = "vizrt/Viz_Datapool_Tutorial/tst";
+            // 
+            // lbpath
+            // 
+            this.lbpath.AutoSize = true;
+            this.lbpath.Location = new System.Drawing.Point(65, 187);
+            this.lbpath.Name = "lbpath";
+            this.lbpath.Size = new System.Drawing.Size(29, 12);
+            this.lbpath.TabIndex = 14;
+            this.lbpath.Text = "Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 496);
+            this.Controls.Add(this.lbpath);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.cmdLoadData);
+            this.Controls.Add(this.cmdLoadscene);
+            this.Controls.Add(this.B_3);
+            this.Controls.Add(this.B_2);
+            this.Controls.Add(this.B_1);
+            this.Controls.Add(this.A_3);
+            this.Controls.Add(this.A_2);
+            this.Controls.Add(this.A_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Connectbtn);
+            this.Controls.Add(this.ConnectPort);
+            this.Controls.Add(this.ConnectIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ConnectIp;
+        private System.Windows.Forms.TextBox ConnectPort;
+        private System.Windows.Forms.Button Connectbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button A_1;
+        private System.Windows.Forms.Button A_2;
+        private System.Windows.Forms.Button A_3;
+        private System.Windows.Forms.Button B_1;
+        private System.Windows.Forms.Button B_2;
+        private System.Windows.Forms.Button B_3;
+        private System.Windows.Forms.Button cmdLoadscene;
+        private System.Windows.Forms.Button cmdLoadData;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Label lbpath;
+    }
+}
+
---
+++ ScoreBoard/ScoreBoard/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VizConnectC;
+
+namespace ScoreBoard
+{
+    public partial class Form1 : Form
+    {
+        VizEnginePool m_EnginePool = new VizEnginePool();
+        int g1Numcount = 0, g2Numcount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connectbtn_Click(object sender, EventArgs e)
+        {
+            m_EnginePool.Disconnect();
+            m_EnginePool.CleanRendererList();
+            m_EnginePool.AddRenderer(ConnectIp.Text, Convert.ToInt16(ConnectPort.Text));
+
+            if (m_EnginePool.Connect()) MessageBox.Show("connect successfully!");
+            else MessageBox.Show("fail to connect the Viz!");
+        }
+
+        private void A_3_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                g1Numcount += 3;
+                m_EnginePool.Send("1 RENDERER*FUNCTION*DataPool*Data SET G1Number = " + g1Numcount.ToString());
+                //光send命令，在很多时候你不知道这条命令的执行情况，故，可以引入request函数。
+
+                string reqBack;
+               // reqBack = m_EnginePool.Request("1 RENDERER*FUNCTION*DataPool*Data SET G1Number = " + g1Numcount.ToString());
+               // MessageBox.Show(reqBack);
+            }
+        }
+
+        private void A_2_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                g1Numcount += 2;
+                m_EnginePool.Send("1 RENDERER*FUNCTION*DataPool*Data SET G1Number = " + g1Numcount.ToString());
+            }
+        }
+
+
+        private void A_1_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                g1Numcount += 1;
+                m_EnginePool.Send("1 RENDERER*FUNCTION*DataPool*Data SET G1Number = " + g1Numcount.ToString());
+            }
+        }
+
+        private void B_1_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                g2Numcount += 1;
+                m_EnginePool.Send("1 RENDERER*FUNCTION*DataPool*Data SET G2Number = " + g2Numcount.ToString());
+            }
+        }
+
+        private void B_2_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                g2Numcount += 2;
+                m_EnginePool.Send("1 RENDERER*FUNCTION*DataPool*Data SET G2Number = " + g2Numcount.ToString());
+            }
+        }
+
+        private void B_3_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                g2Numcount += 3;
+                m_EnginePool.Send("1 RENDERER*FUNCTION*DataPool*Data SET G2Number = " + g2Numcount.ToString());
+            }
+        }
+
+        private void cmdLoadscene_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+               // m_EnginePool.Send("RENDERER SET_OBJECT SCENE*" + path.Text);
+                m_EnginePool.LoadScene(path.Text);
+            }
+        }
+
+        private void cmdLoadData_Click(object sender, EventArgs e)
+        {
+            if (m_EnginePool.IsConnected)
+            {
+                string strBack;
+                strBack = m_EnginePool.Request("1 RENDERER*TREE*@Group1*GEOM*TEXT GET");
+                if (strBack.StartsWith("ERROR"))
+                    MessageBox.Show("Error get the data!");
+                else
+                    g1Numcount = Convert.ToInt16(strBack);
+
+                strBack = m_EnginePool.Request("1 RENDERER*TREE*@Group2*GEOM*TEXT GET");
+                if (strBack.StartsWith("ERROR"))
+                    MessageBox.Show("Error get the data!");
+                else
+                    g2Numcount = Convert.ToInt16(strBack);
+            }
+        }
+    }
+}
